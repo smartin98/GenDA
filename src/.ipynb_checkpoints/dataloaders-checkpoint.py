3b2ba8731a2e62a5f2c3309617f458c986a0880f
smartin98 <s98amartin@gmail.com>
2024-05-29 10:06:10 -0700

    update dataloader to use zarr anomaly files

--- src/.ipynb_checkpoints/dataloaders-checkpoint.py
+++ src/.ipynb_checkpoints/dataloaders-checkpoint.py
@@ -1,94 +1,130 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import xarray as xr
-from src.interp_utils import *
 from datetime import date, timedelta
-import os
 import numpy as np
 
 
-class NeurOST_dataset(Dataset):
+class GAN_dataset(Dataset):
     """
-    A PyTorch Dataset for loading and preparing SSH and SST data for NeurOST inference.
+    A PyTorch Dataset for multi-modal GAN training from GLORYS data.
 
     Attributes:
         data_dir (str): The directory containing all input data files.
-        mid_date (datetime.date): The middle date of the time period to extract data, i.e. the desired reconstruction date.
-        N_t (int): The number of time steps (days) to include in the dataset.
-        mean_ssh (float): Mean SSH value for standardization.
-        std_ssh (float): Standard deviation of SSH values for standardization.
-        mean_sst (float): Mean SST value for standardization.
-        std_sst (float): Standard deviation of SST values for standardization.
-        coord_grids (np.ndarray): Coordinates for the output subdomain grids.
-        n (int, optional): Number of bins in the output grid (default: 128).
-        L_x (float, optional): Longitudinal range of the output grid in meters (default: 960e3).
-        L_y (float, optional): Latitudinal range of the output grid in meters (default: 960e3).
-        filtered_sla (bool, optional): Whether to use filtered sea level anomaly (SLA) data (default: False).
-        withheld_sats (list, optional): List of satellite identifiers to withhold from the data (default: None).
-        sst_zarr_name (str, optional): Name of the Zarr file containing SST data (default: 'foo_bar').
-        multiprocessing (bool, optional): Whether to use multiprocessing for data loading (default: True).
+        latent_dim (int): Dimension of random latent vector inputted to gnerator
+        n_lon (int): Longitude size of NN reconstruction patch
+        n_lat (int): Latitude size of NN reconstruction patch
+        samples_per_day (int): Number of patches to take on each day (randomly distributed)
+        date_range (list): List of 2 datetime objects for start and end date of sampling period.
+        variables (list): List of strings with the names of the data variables to extract from the dataset e.g. 'zos', 'so','thetao', 'uo', 'vo'
+        var_stds (dictionary): Dictionary with std to normalize by for each variable in variables
+        model_zarr_name (str): Name of the Zarr store for the model (within data_dir)
+        lon_buffers (list): Number of pixels in the longitude direction at each edge to exclude from the sampling. None = no buffer.
+        lat_buffers (list): Number of pixels in the latitude direction at each edge to exclude from the sampling. None = no buffer.
+        multiprocessing (bool): Indicates if multi-processing will be used in dataloader. Needed to initialize dataset otherwise it's done in worker_init_fn. (default: True)
         
     Methods:
         __len__(): Returns the number of samples in the dataset.
         worker_init_fn(worker_id): Initializes worker processes for multiprocessing.
         __getitem__(idx): Returns a tuple containing input and output data for the given index.
 
-            Input Data (invar): A tensor of shape (N_t, 2, n, n) containing gridded SST and SSH data.
-            Output Data (outvar): A tensor containing SSH data.
+            Input Data (invar): A tensor of shape latent_dim containing Gaussian random noise for generator input.
+            Output Data (outvar): A tensor of shape (len(variables), n_lon, n_lat) containing extracted Glorys data.
     """
     
-    def __init__(self, data_dir, mid_date, N_t, mean_ssh, std_ssh, mean_sst, std_sst, coord_grids, n = 128, L_x = 960e3, L_y = 960e3, filtered_sla = False, withheld_sats = None, sst_zarr_name = 'foo_bar', multiprocessing = True):
+    def __init__(self, data_dir, latent_dim, n_lon, n_lat, samples_per_day, date_range, variables, var_stds, model_zarr_name, lon_buffers = [None, None], lat_buffers = [None, None], multiprocessing = True):
         self.data_dir = data_dir
-        self.date = date
-        self.N_t = N_t
-        self.mean_ssh = mean_ssh
-        self.std_ssh = std_ssh
-        self.mean_sst = mean_sst
-        self.std_sst = std_sst
-        self.coord_grids = coord_grids
-        self.n = n
-        self.L_x = L_x
-        self.L_y = L_y
-        self.start_date = mid_date - timedelta(days=N_t/2)
-        self.dates = [self.start_date + timedelta(days = t) for t in range(N_t)]
-        ssh_files = GetListOfFiles(data_dir+'cmems_sla/')
-        self.ssh_files = [f for f in ssh_files if date(int(f[-20:-16]),int(f[-16:-14]),int(f[-14:-12])) in self.dates]
-        self.max_outvar_length = 400
-        self.filtered_sla = filtered_sla
-        self.withheld_sats = withheld_sats
-        self.sst_zarr_name = sst_zarr_name
+        self.latent_dim = latent_dim
+        self.n_lon = n_lon
+        self.n_lat = n_lat
+        self.samples_per_day = samples_per_day
+        self.lon_buffers = lon_buffers
+        self.lat_buffers = lat_buffers
+        self.date_range = date_range
+        self.variables = variables
+        self.var_stds = var_stds
+        self.n_channels = len(variables)
+        self.model_zarr_name = model_zarr_name
+        
         if not multiprocessing:
-            self.worker_ds_sst = xr.open_dataset(self.data_dir + self.sst_zarr_name, engine='zarr')
-            self.worker_ds_ssh =load_multisat_ssh(self.ssh_files)
+            self.ds_model = xr.open_zarr(self.data_dir + self.model_zarr_name)
+            
+            i_lon_min = self.lon_buffers[0]
+            i_lon_max = -self.lon_buffers[1]
+            i_lat_min = self.lat_buffers[0]
+            i_lat_max = -self.lat_buffers[1]
+                
+            self.ds_model = self.ds_model.isel(longitude = slice(i_lon_min, i_lon_max), latitude = slice(i_lat_min, i_lat_max), depth = 0, drop = True).sel(time=slice(self.date_range[0],self.date_range[1]), drop = True)
+            
+            self.N_lon = self.ds_model.dims['longitude']
+            self.N_lat = self.ds_model.dims['latitude']
+            self.N_time = self.ds_model.dims['time']
+            
+            # Generate isel arguments
+            self.indexer = []
+            np.random.seed(1)
+            for t in range(self.N_time):
+                lat_indices = np.random.choice(self.N_lat - self.n_lat + 1, self.samples_per_day, replace=False)
+                lon_indices = np.random.choice(self.N_lon - self.n_lon + 1, self.samples_per_day, replace=False)
+                for i in range(lon_indices.shape[0]):# lat_start in lat_indices:
+                    # for lon_start in lon_indices:
+                    self.indexer.append({
+                        'latitude': slice(lat_indices[i], lat_indices[i] + self.n_lat),
+                        'longitude': slice(lon_indices[i], lon_indices[i] + self.n_lon),
+                        'time': t
+                    })
+            
+            
         
     def __len__(self):
-        return self.coord_grids.shape[0]
+        return int(self.samples_per_day*(self.date_range[1]-self.date_range[0]).days)
     
     def worker_init_fn(self, worker_id):
-        self.worker_ds_sst = xr.open_dataset(self.data_dir + self.sst_zarr_name, engine='zarr')
-        self.worker_ds_ssh =load_multisat_ssh(self.ssh_files)
+        self.ds_model = xr.open_dataset(self.data_dir + self.model_zarr_name, engine='zarr')
+
+        i_lon_min = self.lon_buffers[0]
+        if self.lon_buffers[1] is not None:
+            i_lon_max = -self.lon_buffers[1]
+        else:
+            i_lon_max = None
+        i_lat_min = self.lat_buffers[0]
+        if self.lat_buffers[1] is not None:
+            i_lat_max = -self.lat_buffers[1]
+        else:
+            i_lat_max = None
+
+        self.ds_model = self.ds_model.isel(longitude = slice(i_lon_min, i_lon_max), latitude = slice(i_lat_min, i_lat_max), depth = 0, drop = True).sel(time=slice(self.date_range[0],self.date_range[1]), drop = True)
+
+        self.N_lon = self.ds_model.dims['longitude']
+        self.N_lat = self.ds_model.dims['latitude']
+        self.N_time = self.ds_model.dims['time']
+
+        # Generate isel arguments
+        self.indexer = []
+        np.random.seed(1)
+        for t in range(self.N_time):
+            lat_indices = np.random.choice(self.N_lat - self.n_lat + 1, self.samples_per_day, replace=False)
+            lon_indices = np.random.choice(self.N_lon - self.n_lon + 1, self.samples_per_day, replace=False)
+            for i in range(lon_indices.shape[0]):# lat_start in lat_indices:
+                # for lon_start in lon_indices:
+                self.indexer.append({
+                    'latitude': slice(lat_indices[i], lat_indices[i] + self.n_lat),
+                    'longitude': slice(lon_indices[i], lon_indices[i] + self.n_lon),
+                    'time': t
+                })
+        
 
     def __getitem__(self, idx):
-        lon0 = 0.25*(self.coord_grids[idx,int(self.n/2)-1,int(self.n/2)-1,0]+self.coord_grids[idx,int(self.n/2)-1,int(self.n/2),0]+self.coord_grids[idx,int(self.n/2),int(self.n/2)-1,0]+self.coord_grids[idx,int(self.n/2),int(self.n/2),0])
-        lat0 = 0.25*(self.coord_grids[idx,int(self.n/2)-1,int(self.n/2)-1,1]+self.coord_grids[idx,int(self.n/2)-1,int(self.n/2),1]+self.coord_grids[idx,int(self.n/2),int(self.n/2)-1,1]+self.coord_grids[idx,int(self.n/2),int(self.n/2),1])
         
-        sst = grid_sst_hr(self.worker_ds_sst,self.N_t,self.n, self.L_x, self.L_y, lon0, lat0, self.coord_grids[idx,])
-
-        sst[sst!=0] = (sst[sst!=0]-self.mean_sst)/self.std_sst
+        invar = torch.from_numpy(np.random.randn(self.latent_dim))
         
-        if self.withheld_sats is not None:
-            tracks_in, tracks_out = extract_tracked(self.worker_ds_ssh, self.start_date, self.start_date+timedelta(days=self.N_t+1), self.L_x, self.L_y, lon0, lat0, transformer_ll2xyz, self.coord_grids[idx,], self.filtered_sla, withhold_sats = self.withheld_sats)
-        else:
-            tracks_in = extract_tracked(self.worker_ds_ssh, self.start_date, self.start_date+timedelta(days=self.N_t+1), self.L_x, self.L_y, lon0, lat0, transformer_ll2xyz, self.coord_grids[idx,], self.filtered_sla, withhold_sats = self.withheld_sats)
-        ssh_in = grid_ssh(tracks_in, self.n, self.N_t, self.L_x, self.L_y, self.start_date)
+        data_model = self.ds_model.isel(self.indexer[idx])
         
-
-        ssh_in[ssh_in!=0] = (ssh_in[ssh_in!=0]-self.mean_ssh)/self.std_ssh
-        invar = torch.from_numpy(np.stack((sst, ssh_in), axis = 1).astype(np.float32))
-        if self.withheld_sats is not None:
-            ssh_out = reformat_output_tracks(tracks_out, self.max_outvar_length, self.N_t, self.n, self.L_x, self.L_y, self.start_date, self.mean_ssh, self.std_ssh, self.filtered_sla)
-            outvar = torch.from_numpy(ssh_out)
-        else:
-            outvar = torch.from_numpy(np.zeros(1).astype(np.float32))
+        outvar = np.zeros((self.n_channels, self.n_lat, self.n_lon))
+        for v, var in enumerate(self.variables):
+            outvar[v,] = data_model[var]/self.var_stds[var]
+        
+        outvar = torch.from_numpy(outvar.astype(np.float32))
+        outvar = torch.nan_to_num(outvar, nan=0.0)
         
         return invar, outvar
